6c9a209c1b504b0a7e82df0ae896cd59e298c729
Nikolay Bulgakov <brokenbass1990@gmail.com>
2024-09-29 17:23:18 +0300

    fix

--- client/src/components/axiosConfig.js
+++ client/src/components/axiosConfig.js
@@ -5,10 +5,18 @@
 // });
 
 // export default instance;
+
+// import axios from 'axios';
+
+// const instance = axios.create({
+//   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+// });
+
+// export default instance;
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', 
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
 });
 
 export default instance;
